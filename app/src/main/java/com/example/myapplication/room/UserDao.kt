b3e3669baa84671a6c5eba5db2050814b5481f44
kunsang-philips <kunsang.waygal@philips.com>
2021-04-27 22:21:18 +0530

    roomdb before coroutine

--- app/src/main/java/com/example/myapplication/room/UserDao.kt
+++ app/src/main/java/com/example/myapplication/room/UserDao.kt
@@ -9,19 +9,19 @@ import androidx.room.Query
 @Dao
 interface UserDao {
     @Query("SELECT * FROM user")
-    suspend fun getAll(): List<User>
+    fun getAll(): List<User>
 
     @Query("SELECT * FROM user WHERE uid IN (:userIds)")
-    suspend fun loadAllByIds(userIds: IntArray): List<User>
+    fun loadAllByIds(userIds: IntArray): List<User>
 
     @Query(
         "SELECT * FROM user WHERE first_name LIKE :first AND " +
             "last_name LIKE :last LIMIT 1"
     )
-    suspend fun findByName(first: String, last: String): User
+    fun findByName(first: String, last: String): User
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAll(users: List<User>)
+    fun insertAll(users: List<User>)
 
     @Delete
     fun delete(user: User)
--- app/src/main/java/com/example/myapplication/ui/main/RoomDBActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/main/RoomDBActivity.kt
@@ -1,15 +1,14 @@
 package com.example.myapplication.ui.main
 
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import com.example.myapplication.R
 import com.example.myapplication.room.AppDatabase
 import com.example.myapplication.room.User
 import kotlinx.android.synthetic.main.activity_room_d_b.buttonFetch
 import kotlinx.android.synthetic.main.activity_room_d_b.textView
-import kotlinx.coroutines.launch
 
 class RoomDBActivity : AppCompatActivity() {
     private lateinit var db: AppDatabase
@@ -19,21 +18,23 @@ class RoomDBActivity : AppCompatActivity() {
         setContentView(R.layout.activity_room_d_b)
         initializeRoomDB()
         buttonFetch.setOnClickListener {
-            lifecycleScope.launch {
+            textView.text = ""
+            AsyncTask.execute {
                 insertUsers()
                 displayUser()
             }
         }
     }
 
-    private suspend fun displayUser() {
-        textView.text = ""
+    private fun displayUser() {
         db.userDao().getAll().forEach {
-            textView.append("${it.firstName} ${it.lastName}\n")
+            runOnUiThread {
+                textView.append("${it.firstName} ${it.lastName}\n")
+            }
         }
     }
 
-    private suspend fun insertUsers() {
+    private fun insertUsers() {
         val list = mutableListOf<User>()
         list.add(User(1, "Virat", "Kohli"))
         list.add(User(2, "Rohit", "Sharma"))
