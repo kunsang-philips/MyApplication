9d0488d89d95764d8fd2ca15fc306bcb40ce7a9d
kunsang-philips <kunsang.waygal@philips.com>
2021-04-29 19:52:08 +0530

    unit testing commit

---
+++ app/src/main/java/com/example/myapplication/unitTesting/CoroutineTestActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.unitTesting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+import com.example.myapplication.unitTesting.ui.main.CoroutineTestFragment
+
+class CoroutineTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.coroutine_test_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, CoroutineTestFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/unitTesting/ui/main/CoroutineTestViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.unitTesting.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class CoroutineTestViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
